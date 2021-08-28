da3751b3585c7486a55c6281ae4981116f596cb6
Tabris <tabris.zhuxun913@gmail.com>
2021-08-28 00:33:53 -0700

    initialize backend

---
+++ backend/src/main/java/com/pivottech/booking/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.pivottech.booking;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello World!";
+    }
+}
